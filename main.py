ce5ce41711209aa258b80df9953a18cdd98f81e1
rickyegl <a01287576@tec.mx>
2024-11-28 10:24:49 -0600

    added not 0 check

--- main.py
+++ main.py
@@ -287,7 +287,7 @@ def main():
         observation_, reward, done_, trun_, info = env.step_wait()
         done_ = np.logical_or(done_, trun_)
 
-        if(reward>=max(scores_count)):
+        if(reward>=max(scores_count) and reward>0):
             agent.save_model()
             
         for i in range(num_envs):
